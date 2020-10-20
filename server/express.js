241adba8a4665addd50c86db30a4c41334833edc
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 00:39:56 +1100

    Configure express app with middleware modules

---
+++ server/express.js
@@ -0,0 +1,54 @@
+import express from "express";
+import devBundle from "./devBundle";
+import path from "path";
+import template from "../template";
+
+// Express middleware modules
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
+
+const CURRENT_WORKING_DIR = process.cwd();
+const mongoDBUrl =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/mongodb";
+
+const app = express();
+
+// Serve static files from dist folder when the requested route starts with /dist
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+// --------------------- MIDDLEWARE MODULES -------------------
+
+/*
+Request body-parsing middleware to handle the complexities of 
+parsing streamable request objects so that we can simplify 
+browser-server communication by exchanging JSON in the request body. 
+*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cookie parsing middleware to parse and set cookies in request objects.
+app.use(cookieParser());
+
+// will attempt to compress response bodies for all requests that traverse through the middleware.
+app.use(compress());
+
+// Collection of middleware functions to help secure Express apps by setting various HTTP headers.
+app.use(helmet());
+
+// Middleware to enable cross-origin resource sharing (CORS).
+app.use(cors());
+
+// --------------------------------
+
+// COMMENT THIS OUT FOR PRODUCTION
+devBundle.compile(app);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,38 +1,17 @@
-import express from "express";
-import devBundle from "./devBundle";
-import path from "path";
-import template from "../template";
-import { MongoClient } from 'mongodb'
-
-const CURRENT_WORKING_DIR = process.cwd();
-const mongoDBUrl = process.env.MONGODB_URI ||
-  'mongodb://localhost:27017/mongodb'
-
-const app = express();
-
-// Serve static files from dist folder when the requested route starts with /dist
-app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
-
-// COMMENT THIS OUT FOR PRODUCTION
-devBundle.compile(app);
-
-// ROUTES
-app.get("/", (req, res) => {
-  res.status(200).send(template());
-});
+import config from "./../config/config";
+import app from "./express";
+import { MongoClient } from "mongodb";
 
 // Start server to listen for incoming requests
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.info("Server started on port %s.", port);
+  console.info("Server started on port %s.", config.port);
 });
 
-
 // Connect to MongoDB
-MongoClient.connect(mongoDBUrl, (err, db)=>{
-  console.log("Connected successfully to mongodb server")
- db.close()
-})
+MongoClient.connect(mongoDBUrl, (err, db) => {
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
