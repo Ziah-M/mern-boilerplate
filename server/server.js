9ff0323e80a52c3b1309ddacf610d9242acc1926
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-20 23:20:10 +1100

    Configure the server
    implement the Node-Express app
    -- initiates client-side code bundling
    -- starts the server
    -- sets up the path to serve static assets to the client
    --Renders the React view in a template when a GET request is made to the root route

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import devBundle from "./devBundle";
+import path from "path";
+import template from "../template";
+
+const CURRENT_WORKING_DIR = process.cwd();
+
+const app = express();
+
+// Serve static files from dist folder when the requested route starts with /dist
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+// COMMENT THIS OUT FOR PRODUCTION
+devBundle.compile(app);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+// Start server to listen for incoming requests
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", port);
+});
