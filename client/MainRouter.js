4119a09285ddd64c4f7d0d192492ae05bd23bc9c
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 23:12:18 +1100

    Add Users view component to list the users

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+
 const MainRouter = () => {
   return (
     <div>
       <Switch>
+        <Route path="/users" component={Users} />
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
