77e292f048f4c88f19d4229d0148c4367a5c2919
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 22:45:58 +1100

    Add auth helper methods for managing the JWT

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,29 @@
+import { signOut } from "./api-auth";
+
+function authenticate(jwt = "", cb = (f) => f) {
+  // ensure code is running in a browser and has access to session Storage
+  if (typeof window !== "undefined")
+    sessionStorage.setItem("jwt", JSON.stringify(jwt));
+  cb();
+}
+
+// returns either the stored credentials or false
+function isAuthenticated() {
+  if (typeof window == "undefined") return false;
+
+  if (sessionStorage.getItem("jwt"))
+    return JSON.parse(sessionStorage.getItem("jwt"));
+  else return false;
+}
+
+function clearJWT(cb) {
+  if (typeof window !== "undefined") sessionStorage.removeItem("jwt");
+
+  cb();
+  // clears the cookie in case cookie is used for auth
+  signout().then((data) => {
+    document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  });
+}
+
+export { authenticate, isAuthenticated, clearJWT };
