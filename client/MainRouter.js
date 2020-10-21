d526408dbdee40bd59142eac646b864fcb972844
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 23:23:04 +1100

    Add Sign Up form view

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import SignUp from "./user/Signup";
+import Users from "./user/Users";
 
 const MainRouter = () => {
   return (
     <div>
       <Switch>
         <Route path="/users" component={Users} />
+        <Route path="/signup" component={Signup} />
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
