7967ac02e7abff7be6d46d696f56d698f196a538
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 23:52:05 +1100

    Add profile view

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,12 +4,14 @@ import Home from "./core/Home";
 import SignUp from "./user/Signup";
 import Users from "./user/Users";
 import SignIn from "./auth/SignIn";
+import Profile from "./user/Profile";
 
 const MainRouter = () => {
   return (
     <div>
       <Switch>
         <Route path="/users" component={Users} />
+        <Route path="/user/:userId" component={Profile} />
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={SignIn} />
         <Route exact path="/" component={Home} />
