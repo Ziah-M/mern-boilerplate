0ba20ad00978d66a5c0e9c4d2691fb1d9fd82f45
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-21 23:38:38 +1100

    Add Sign In View

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import SignUp from "./user/Signup";
 import Users from "./user/Users";
+import SignIn from "./auth/SignIn";
 
 const MainRouter = () => {
   return (
@@ -10,6 +11,7 @@ const MainRouter = () => {
       <Switch>
         <Route path="/users" component={Users} />
         <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={SignIn} />
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
